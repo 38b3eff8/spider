01489cd6e8f055278bd6ffa9e8ee5d97eae1ebb6
zhouyifan <552094467@qq.com>
2016-08-10 16:54:54 +0800

    modify something

--- fabfile.py
+++ fabfile.py
@@ -3,13 +3,14 @@ from fabric.api import local, cd, env, run
 env.hosts = ['zhouyifan.top']
 
 
-
 def prepare_deploy():
     commit()
     push()
 
+
 def push():
     local('git push')
+
 
 def commit():
     local('git add . && git commit -m "modify something"')
