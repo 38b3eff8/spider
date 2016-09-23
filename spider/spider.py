dfa0c2e23c4293e42fabbdb2f235db6c0317d5d9
zhouyifan <552094467@qq.com>
2016-09-23 16:05:28 +0800

    change

--- spider/spider.py
+++ spider/spider.py
@@ -27,6 +27,12 @@ class Spider(object):
 
         return _wrapper
 
+    def run(self):
+        worker = config['base']['worker']
+        for i in range(worker):
+            t = threading.Thread(target=create_worker(i, self, response))
+            t.start()
+
     def proxy(self, func):
         self.get_proxy = func
 
@@ -34,12 +40,6 @@ class Spider(object):
             return func()
 
         return _wrapper
-
-    def run(self):
-        worker = config['base']['worker']
-        for i in range(worker):
-            t = threading.Thread(target=create_worker(i, self, response))
-            t.start()
 
     def filter(self, include=None, exclude=None):
         if include:
